58fc65b5debdb3274dd103b44e52b3031bd51fd8
Keith Presley <80120339@bucks.ac.uk>
2019-02-03 11:49:54 +0000

    Added Add/View Property to Manage Account Page

---
+++ ASPNET360SIte/Pages/Properties/Create.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ASPNET360SIte.Models;
+using ASPNET360SIte.Models.IdentityCustomisationTest.Models;
+using System.IO;
+
+namespace ASPNET360SIte.Pages.Properties
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ASPNET360SIte.Models.ASPNET360SIteContext _context;
+
+        public CreateModel(ASPNET360SIte.Models.ASPNET360SIteContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Property Property { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var path = Path.Combine(
+            Directory.GetCurrentDirectory(), "wwwroot/uploads",
+            Property.MainImage.FileName);
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await Property.MainImage.CopyToAsync(stream);
+                Property.MainImagePath = Property.MainImage.FileName;
+            }
+
+            _context.Property.Add(Property);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
