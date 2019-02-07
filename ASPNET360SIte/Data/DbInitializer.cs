9eed022c1a2ed21bb4ca01e2796a12b616f1dbe1
Keith Presley <80120339@bucks.ac.uk>
2019-02-07 21:01:26 +0000

    AddedDataSetForLoading

--- ASPNET360SIte/Data/DbInitializer.cs
+++ ASPNET360SIte/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using ASPNET360SIte.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,6 @@ namespace ASPNET360SIte.Data
 {
     public class DbInitializer
     {
-        /*
         public static void Initialize(ASPNET360SIteContext context)
         {
             // context.Database.EnsureCreated();
@@ -21,14 +21,17 @@ namespace ASPNET360SIte.Data
             var properties = new Property[]
             {
             new Property{Build="11",Street="The Roperies",Locality="Hatters Lane",Town="High Wycombe",County="Buckinghamshire",PostCode="HP13 7FW",Country="Great Britain"},
+
             new Property{Build="5",Street="Radcliffe Road",Locality="Lupset",Town="Wakefield",County="West Yorkshire",PostCode="WF2 8RS",Country="Great Britain"},
+			/*
             new Property{Build="80",Street="Cressex Road",Locality="Cressex",Town="High Wycombe",County="Buckinghamshire",PostCode="HP12 4TU",Country="Great Britain"},
             new Property{Build="15",Street="Denmark Street",Locality="Fenny Strafford",Town="Bletchley",County="Buckinghamshire",PostCode="MK2 2NH",Country="Great Britain"},
             new Property{Build="59",Street="Priory Avenue",Locality="Hughenden",Town="High Wycombe",County="Buckinghamshire",PostCode="HP13 6SN",Country="Great Britain"},
             new Property{Build="38",Street="Scott Road",Locality="Pitsmoor",Town="Sheffield",County="South Yorkshire",PostCode="S6 3Ax",Country="Great Britain"},
             new Property{Build="181",Street="West Wycombe Road ",Locality="Downley",Town="High Wycombe",County="Buckinghamshire",PostCode="HP13 8AN",Country="Great Britain"},
-            new Property{Build="33",Street="Danvers Roads",Locality="Narboorugh",Town="Leicester",County="Liecestershire",PostCode="LE3 2AD",Country="Great Britain"},
+            new Property{Build="33",Street="Danvers Roads",Locality="Narborough",Town="Leicester",County="Leicestershire",PostCode="LE3 2AD",Country="Great Britain"},
             //new Property{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+			*/
             };
             foreach (Property s in properties)
             {
@@ -37,43 +40,66 @@ namespace ASPNET360SIte.Data
             context.SaveChanges();
 
             var floorPlans = new FloorPlan[]
-            {
-            new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",=1},
-            new FloorPlan{CourseID=4022,Title="Microeconomics",Credits=3},
-            new FloorPlan{CourseID=4041,Title="Macroeconomics",Credits=3},
-            new FloorPlan{CourseID=1045,Title="Calculus",Credits=4},
-            new FloorPlan{CourseID=3141,Title="Trigonometry",Credits=4},
-            new FloorPlan{CourseID=2021,Title="Composition",Credits=3},
-            new FloorPlan{CourseID=2042,Title="Literature",Credits=4}
-            };
+             {
+            new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=1},
+            new FloorPlan{FloorPlanLabel="First Floor",MainImagePath="",CustomPropertyID=1},
+            new FloorPlan{FloorPlanLabel="Second Floor",MainImagePath="",CustomPropertyID=1},
+
+            new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=2},
+            new FloorPlan{FloorPlanLabel="First Floor",MainImagePath="",CustomPropertyID=2},
+                 /*
+                 new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=3},
+                 new FloorPlan{FloorPlanLabel="First Floor",MainImagePath="",CustomPropertyID=3},
+                 new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=4},
+                 new FloorPlan{FloorPlanLabel="First Floor",MainImagePath="",CustomPropertyID=4},
+                 new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=5},
+                 new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=6},
+                 new FloorPlan{FloorPlanLabel="First Floor",MainImagePath="",CustomPropertyID=6},
+                 new FloorPlan{FloorPlanLabel="First Floor Flat",MainImagePath="",CustomPropertyID=7},
+                 new FloorPlan{FloorPlanLabel="Ground Floor",MainImagePath="",CustomPropertyID=7},
+                 new FloorPlan{FloorPlanLabel="First Floor",MainImagePath="",CustomPropertyID=8},
+                 */
+             };
             foreach (FloorPlan c in floorPlans)
             {
                 context.FloorPlan.Add(c);
             }
             context.SaveChanges();
 
-            var enrollments = new Enrollment[]
+            var rooms = new Room[]
             {
-            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
-            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
-            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
-            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
-            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
-            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
-            new Enrollment{StudentID=3,CourseID=1050},
-            new Enrollment{StudentID=4,CourseID=1050},
-            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
-            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
-            new Enrollment{StudentID=6,CourseID=1045},
-            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            new Room{RoomLabel="Hallway",MainImagePath="",CustomFloorPlanID=1},
+            new Room{RoomLabel="Living Room",MainImagePath="",CustomFloorPlanID=1},
+            new Room{RoomLabel="Dinning Room",MainImagePath="",CustomFloorPlanID=1},
+            new Room{RoomLabel="Kitchen",MainImagePath="",CustomFloorPlanID=1},
+            new Room{RoomLabel="Ground Floor Toilet",MainImagePath="",CustomFloorPlanID=1},
+            new Room{RoomLabel="Garden",MainImagePath="",CustomFloorPlanID=1},
+            new Room{RoomLabel="First Floor Master Bedroom (1) ",MainImagePath="",CustomFloorPlanID=2},
+            new Room{RoomLabel="On Suite Bathroom",MainImagePath="",CustomFloorPlanID=2},
+            new Room{RoomLabel="First Floor Bedroom (2)",MainImagePath="",CustomFloorPlanID=2},
+            new Room{RoomLabel="First Floor Hallway",MainImagePath="",CustomFloorPlanID=2},
+            new Room{RoomLabel="Second Floor Bedroom (1)",MainImagePath="",CustomFloorPlanID=3},
+            new Room{RoomLabel="Second Floor Bedroom (2)",MainImagePath="",CustomFloorPlanID=3},
+            new Room{RoomLabel="Second Floor Bathroom",MainImagePath="",CustomFloorPlanID=3},
+            new Room{RoomLabel="Second Floor Hallway",MainImagePath="",CustomFloorPlanID=3},
+
+            new Room{RoomLabel="Hallway",MainImagePath="",CustomFloorPlanID=4},
+            new Room{RoomLabel="Living Room",MainImagePath="",CustomFloorPlanID=4},
+            new Room{RoomLabel="Dinning Room",MainImagePath="",CustomFloorPlanID=4},
+            new Room{RoomLabel="Kitchen",MainImagePath="",CustomFloorPlanID=4},
+            new Room{RoomLabel="Ground Floor Toilet",MainImagePath="",CustomFloorPlanID=4},
+            new Room{RoomLabel="Garden",MainImagePath="",CustomFloorPlanID=4},
+            new Room{RoomLabel="First Floor Bedroom (1) ",MainImagePath="",CustomFloorPlanID=5},
+            new Room{RoomLabel="First Floor Bedroom (2)",MainImagePath="",CustomFloorPlanID=5},
+            new Room{RoomLabel="First Floor Hallway",MainImagePath="",CustomFloorPlanID=5},
+            new Room{RoomLabel="First Floor Bathroom",MainImagePath="",CustomFloorPlanID=5},
             };
-            foreach (Enrollment e in enrollments)
+            foreach (Room e in rooms)
             {
-                context.Enrollment.Add(e);
+                context.Room.Add(e);
             }
             context.SaveChanges();
         }
-        */
     }
 }
 
